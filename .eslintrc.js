7b98e1f307e201cc0b6bb935fed642b28ae5243d
philastrio <han_gang@naer.com>
2019-03-01 06:32:20 +0900

    graphql base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,1 @@
-module.exports = {
-  extends: 'airbnb-base',
-  rules: {
-    'linebreak-style': 'off',
-    'no-tabs': 'off',
-    'no-mixed-spaces-and-tabs': 'off',
-    'comma-dangle': 'off',
-    'no-trailing-spaces': 'off',
-    'no-unused-vars': 'off',
-    'implicit-arrow-linebreak': 'off',
-    'react/jsx-filename-extension': 'off',
-    'react/jsx-one-expression-per-line': 'off',
-    'no-confusing-arrow': 'off',
-    'object-curly-newline': 'off',
-    'ts-styled-plugin(9999)': 'off'
-  }
-};
+module.exports = {};
--- graphql/db.js
+++ graphql/db.js
@@ -1,16 +1,45 @@
-export const people = [
-  { id: 0, name: 'Hankang', age: 30, gender: 'female' },
-  { id: 1, name: 'arom', age: 23, gender: 'male' },
-  { id: 2, name: 'osun', age: 60, gender: 'female' },
-  { id: 3, name: 'jihui', age: 1, gender: 'female' },
-  { id: 4, name: 'ku', age: 65, gender: 'male' },
-  { id: 5, name: 'hyuna', age: 34, gender: 'female' },
-  { id: 6, name: 'junghi', age: 36, gender: 'male' },
-  { id: 7, name: 'Yong', age: 43, gender: 'male' },
-  { id: 8, name: 'sanghyuk', age: 64, gender: 'male' }
-];
+import axios from 'axios';
+const BASE_URL = 'https://yts.am/api/v2/';
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === id);
-  return filteredPeople[0];
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    }
+  });
+  return movies;
+};
+
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movie;
+};
+
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movies;
 };
