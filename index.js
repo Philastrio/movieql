6b79cd9b16d007599d94109c21a44c47c6a8d4e6
philastrio <han_gang@naer.com>
2019-03-02 05:20:53 +0900

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { GraphQLServer } from 'graphql-yoga';
 import resolvers from './graphql/resolvers';
 
+const url = 'https://hkmovieql.herokuapp.com/';
+
 const server = new GraphQLServer({
   typeDefs: 'graphql/schema.graphql',
   resolvers
